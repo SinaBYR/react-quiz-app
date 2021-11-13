e1ae9f450190d36cff6f56b3a7a884b690a3e12d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-14 00:09:10 +0330

    Add Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,16 @@
 import { LayoutStyled } from "./LayoutStyled"
 import { Showcase } from "../Showcase/Showcase"
 import { Switch, Route } from 'react-router-dom';
+import { Footer } from "../Footer/Footer";
 
-const Layout = () => {
+const Layout: () => JSX.Element = () => {
     return (
         <LayoutStyled>
             <Switch>
                 <Route path="/" component={Showcase}/>
-                <Route path="/start" />
+                {/* <Route path="/start" /> */}
             </Switch>
-            
+            <Footer />
         </LayoutStyled>
     )
 }
--- src/components/Layout/LayoutStyled.tsx
+++ src/components/Layout/LayoutStyled.tsx
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
 
 export const LayoutStyled = styled.div`
-    min-height: 100vh;
     height: 100%;
-    background-color: #FFEDDA;
+    min-height: 100vh;
+    background-color: #FFB830;
+    background-color: #172774;
+    background-color: #FFEDD3;
+    background-color: #77D970;
+    background-color: #FF2442;
+    background-color: #D0CAB2;
+    color: #FFEDDA;
+    color: #3DB2FF;
+    color: #8a0b46;
+    color: #96C7C1;
+    color: #FF5C58;
+    color: #2d534e;
+    
 `
+
+// 041b2a
