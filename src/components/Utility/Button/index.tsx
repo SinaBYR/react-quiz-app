5ee7a0d14a581530a02a7bca708d94dfab0b517d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-14 18:22:52 +0330

    Add custom styled buttons

--- src/components/Utility/Button/index.tsx
+++ src/components/Utility/Button/index.tsx
@@ -1,20 +1,22 @@
 import styled from "styled-components";
 
-interface StyledButtonProps {
-    kind: 'primary' | 'secondary' | 'warning';
-}
-
-type Kind ='primary' | 'secondary' | 'warning'; 
+type Kind ='primary' | 'secondary' | 'warning' | 'success'; 
 
 const ButtonStyled = styled.button<{ kind: Kind}>`
     background-color: ${({ kind }) => {
-        if(kind === 'primary') {
-            return '#eeeeee'
-        } else if(kind === 'secondary') {
-            return '#3DB2FF'
-        } else if(kind === 'warning') {
-            return '#FF2442'
+        switch(kind) {
+            case 'primary': return '#eeeeee'
+            case 'secondary': return '#544179'
+            case 'success': return '#146310'
+            case 'warning': return '#FF2442'
         }
+    }};
+    color: ${({ kind }) => {
+        if(kind === 'primary') {
+            return '#080808'
+        }
+
+        return '#FFFFFF'
     }};
     padding: 1rem 1.5rem;
     border: none;
@@ -22,7 +24,6 @@ const ButtonStyled = styled.button<{ kind: Kind}>`
     font-size: 1rem;
     display: block;
     border-radius: 4px;
-    box-shadow: 0 0 4px #2c2c2c;
 
     &:active {
         transform: scale(0.98);
@@ -32,3 +33,14 @@ const ButtonStyled = styled.button<{ kind: Kind}>`
 export const Button: React.FC<{ type: Kind, children: string }> = ({ type, children}) => {
     return <ButtonStyled kind={type}>{children}</ButtonStyled>
 }
+
+// return '#8a0b46'
+// return '#00A19D'
+// return '#FF5F7E'
+// return '#EEEEEE'
+// return '#3DB2FF'
+// return '#FFBF86'
+// return '#49FF00'
+// return '#911F27'
+// return '#C6D57E'
+// return '#2FDD92'
