f994ae50c5fe99d1e72bc47a3d5f7723feb67ccf
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-16 19:57:31 +0330

    Change theme file

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,15 @@
-interface Theme {
+export interface Theme {
     colors: {
         primary: string;
         secondary: string;
+        alt: string;
     }
 }
 
 export const theme: Theme = {
     colors: {
         primary: '#544179',
-        secondary: '#FFAB4C'
+        secondary: '#FFAB4C',
+        alt: '#8a0b46'
     }
 }
