d1f4934410e0bd537d66d29d5519dc682af9e26e
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-19 20:18:22 +0330

    Change color palette

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,14 +2,21 @@ export interface Theme {
     colors: {
         primary: string;
         secondary: string;
-        alt: string;
+        teritary: string;
     }
 }
 
 export const theme: Theme = {
     colors: {
-        primary: '#544179',
-        secondary: '#FFAB4C',
-        alt: '#8a0b46'
+        primary: '#435560',
+        secondary: '#F6EABE',
+        teritary: '#F7F6F2',
     }
 }
+// export const theme: Theme = {
+//     colors: {
+//         primary: '#544179',
+//         secondary: '#FFAB4C',
+//         alt: '#8a0b46'
+//     }
+// }
