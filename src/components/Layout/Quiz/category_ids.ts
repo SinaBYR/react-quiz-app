13fc34dd17688afc1324abfb757056a8e99b0f5a
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-21 20:49:59 +0330

    Add category ids

---
+++ src/components/Layout/Quiz/category_ids.ts
@@ -0,0 +1,9 @@
+// On opentb.com api, categories have ids. To select an specific category, id of a category must be included in URL instead of name of the category.
+
+export const CATEGORY_IDS = {
+    general: '9',
+    movies: '11',
+    music: '12',
+    videoGames: '15',
+    sports: '21'
+}
