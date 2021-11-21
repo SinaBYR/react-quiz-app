a4ced13aec9963e663597902a1a471d2e76974c6
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-21 14:21:29 +0330

    Initialize reducer function and action types

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { ReactElement } from "react";
+import { createContext, Dispatch, ReactElement, useReducer } from "react";
 import { Layout } from "./components/Layout/Layout"
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme/theme";
+import { initialState, InitialStateType, reducer } from "./store/reducer";
+import { ACTIONTYPE } from "./store/actions";
+
+// dispatch and state contexts
+const DispatchContext = createContext<Dispatch<ACTIONTYPE> | null>(null)
+const StateContext = createContext<InitialStateType | null>(null)
 
 const App: () => ReactElement = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
-        <ThemeProvider theme={theme}>
-            <Layout />
-        </ThemeProvider>
+        <DispatchContext.Provider value={dispatch}>
+            <StateContext.Provider value={state}>
+                <ThemeProvider theme={theme}>
+                    <Layout />
+                </ThemeProvider>
+            </StateContext.Provider>
+        </DispatchContext.Provider>
     )
 }
 
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,4 +1,26 @@
+import { useReducer } from "react"
+import { ACTIONTYPE } from './actions'
 
-export const reducer = () => {
-    return false
+export interface InitialStateType {
+    count: number;
+    page: 'setup' | 'game';
+}
+
+export const initialState: InitialStateType = {
+    count: 0,
+    page: 'setup'
+}
+
+export function reducer(state = initialState, action: ACTIONTYPE) {
+    switch (action.type) {
+        case 'increment': {
+            return { ...state, count: state.count + 1 }
+        }
+        case 'decrement': {
+            return { ...state, count: state.count - 1 }
+        }
+        default: {
+            return state
+        }
+    }
 }
