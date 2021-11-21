b93bf22f4c83138399f143aa02b06aedec101284
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-21 22:17:32 +0330

    Add Result type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,21 @@
 export interface HTMLElementAttributes extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactElement | React.ReactElement[]
 }
+
+export interface FORMDATA {
+    category: 'general' | 'movies' | 'music' | 'sports' | 'videoGames';
+    // noq = number of questions
+    noq: 5|10|15|20;
+    time: 0|1|2|5;
+    difficulty: 'easy' | 'medium' | 'hard'
+}
+
+// Structure of each question result coming from opentdb api
+export interface Result {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
