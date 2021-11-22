729187605bc0a4a196e2ce0d7ae337844d70aeed
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-22 14:07:01 +0330

    Update quiz and setup files

--- src/components/Layout/Quiz/Quiz.tsx
+++ src/components/Layout/Quiz/Quiz.tsx
@@ -1,11 +1,48 @@
+import { useContext, useState } from "react";
+import { axios } from "../../../axios/axios";
+import { DispatchContext } from "../../../store/context";
+import { FORMDATA, Result } from "../../../types/types";
+import { CATEGORY_IDS } from "./category_ids";
 import { Game } from "./Game/Game";
 import { Setup } from "./Setup/Setup";
 
 export const Quiz = () => {
-    return (
-        <>
-            <Game />
-            <Setup />
-        </>
-    )
+    const [page, setPage] = useState<'setup' | 'game'>('setup');
+    // const [data, setData] = useState<Result[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const dispatch = useContext(DispatchContext)
+
+    const changePageHandler = () => {
+        if(page === 'setup') {
+            return setPage('game')
+        }
+
+        setPage('setup')
+    }
+
+    const startGame = async (config: FORMDATA) => {
+        setLoading(true)
+        console.log(config)
+        try {
+            const url = `?amount=${config.noq}&category=${CATEGORY_IDS[config.category]}&difficulty=${config.difficulty}&type=multiple`
+            const response = await axios.get(url)
+            const resdata = await response.data
+
+            if(!resdata.results.length) {
+                return
+            }
+
+            // setData(resdata.results)
+            // console.log(url)
+            dispatch({ type: 'store_data', payload: resdata.results})
+            // console.log(resdata)
+            changePageHandler()
+            setLoading(false)
+        } catch(err) {
+            console.log(err)
+            setLoading(false)
+        }
+    }
+
+    return page === 'setup' ? <Setup start={startGame} loading={loading}/> : <Game />
 }
--- src/components/Layout/Quiz/Setup/Setup.tsx
+++ src/components/Layout/Quiz/Setup/Setup.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement } from "react";
 import styled from "styled-components";
+import { ChangeEvent, useState } from "react";
+import { FORMDATA } from "../../../../types/types";
 import { Button, RadioInput } from "../../../Utility";
 import { Section } from "./Section/Section";
 import { SectionWrapper } from "./Section/SectionWrapper/SectionWrapper";
+import { BeatLoader } from 'react-spinners';
 
 const SetupStyled = styled.div`
     width: 100%;
@@ -17,39 +19,66 @@ const SetupStyled = styled.div`
     }
 `
 
-export const Setup: () => ReactElement = () => {
+export const Setup = ({ start, loading }: { start: (config: FORMDATA) =>  void; loading: boolean}) => {
+    const [formData, setFormData] = useState<FORMDATA>({
+        category: 'general',
+        noq: '10',
+        time: '2',
+        difficulty: 'medium'
+    })
+
+    const onChangeHandler = (e: ChangeEvent) => {
+        setFormData({
+            ...formData,
+            [e.target.getAttribute('name')!]: e.target.getAttribute('value')
+        })
+        console.log(formData.time)
+    }
+
     return (
         <SetupStyled>
             <h1 style={{textAlign: 'center'}}>Quiz Setup</h1>
             <Section>
                 <h2>Category</h2>
                 <SectionWrapper>
-                    <RadioInput type="radio" value="movies" name="category">General</RadioInput>
-                    <RadioInput type="radio" value="movies" name="category">Movies</RadioInput>
-                    <RadioInput type="radio" value="movies" name="category">Music</RadioInput>
-                    <RadioInput type="radio" value="movies" name="category">Sports</RadioInput>
-                    <RadioInput type="radio" value="movies" name="category">Video Games</RadioInput>
+                    <RadioInput type="radio" value="general" checked={formData.category === 'general'} name="category" onChange={onChangeHandler}>General</RadioInput>
+                    <RadioInput type="radio" value="movies" checked={formData.category === 'movies'} name="category" onChange={onChangeHandler}>Movies</RadioInput>
+                    <RadioInput type="radio" value="music" checked={formData.category === 'music'} name="category" onChange={onChangeHandler}>Music</RadioInput>
+                    <RadioInput type="radio" value="sports" checked={formData.category === 'sports'} name="category" onChange={onChangeHandler}>Sports</RadioInput>
+                    <RadioInput type="radio" value="video-games" checked={formData.category === 'video-games'} name="category" onChange={onChangeHandler}>Video Games</RadioInput>
                 </SectionWrapper>
             </Section>
             <Section>
                 <h2>Number of questions</h2>
                 <SectionWrapper>
-                    <RadioInput type="radio" value="5" name="number-of-questions">5</RadioInput>
-                    <RadioInput type="radio" value="10" name="number-of-questions">10</RadioInput>
-                    <RadioInput type="radio" value="15" name="number-of-questions">15</RadioInput>
-                    <RadioInput type="radio" value="20" name="number-of-questions">20</RadioInput>
+                    <RadioInput type="radio" name="noq" value="5" checked={formData.noq === '5'} onChange={onChangeHandler}>5</RadioInput>
+                    <RadioInput type="radio" name="noq" value="10" checked={formData.noq === '10'} onChange={onChangeHandler}>10</RadioInput>
+                    <RadioInput type="radio" name="noq" value="15" checked={formData.noq === '15'} onChange={onChangeHandler}>15</RadioInput>
+                    <RadioInput type="radio" name="noq" value="20" checked={formData.noq === '20'} onChange={onChangeHandler}>20</RadioInput>
+                </SectionWrapper>
+            </Section>
+            <Section>
+                <h2>Difficulty</h2>
+                <SectionWrapper>
+                    <RadioInput type="radio" name="difficulty" value="easy" checked={formData.difficulty === 'easy'} onChange={onChangeHandler}>Easy</RadioInput>
+                    <RadioInput type="radio" name="difficulty" value="medium" checked={formData.difficulty === 'medium'} onChange={onChangeHandler}>Medium</RadioInput>
+                    <RadioInput type="radio" name="difficulty" value="hard" checked={formData.difficulty === 'hard'} onChange={onChangeHandler}>Hard</RadioInput>
                 </SectionWrapper>
             </Section>
             <Section>
                 <h2>Time</h2>
                 <SectionWrapper>
-                    <RadioInput type="radio" value="none" name="time">No time</RadioInput>
-                    <RadioInput type="radio" value="1" name="time">1 min</RadioInput>
-                    <RadioInput type="radio" value="2" name="time">2 min</RadioInput>
-                    <RadioInput type="radio" value="5" name="time">5 min</RadioInput>
+                    <RadioInput type="radio" name="time" value="0" checked={formData.time === '0'} onChange={onChangeHandler}>No time</RadioInput>
+                    <RadioInput type="radio" name="time" value="1" checked={formData.time === '1'} onChange={onChangeHandler}>1 min</RadioInput>
+                    <RadioInput type="radio" name="time" value="2" checked={formData.time === '2'} onChange={onChangeHandler}>2 min</RadioInput>
+                    <RadioInput type="radio" name="time" value="5" checked={formData.time === '5'} onChange={onChangeHandler}>5 min</RadioInput>
                 </SectionWrapper>
             </Section>
-            <Button style={{margin: '0 auto'}} type="secondary">Start Now</Button>
+            <Button style={{margin: '0 auto'}} type="secondary" onClick={() => start(formData)}>
+                {
+                    loading ? <BeatLoader /> : 'Start Now'
+                }
+            </Button>
         </SetupStyled>
     )
 }
