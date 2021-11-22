58779d2053245097781dc3532adb5834873ed591
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-22 11:28:38 +0330

    Add store quiz data case to the reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,23 +1,25 @@
-import { useReducer } from "react"
-import { ACTIONTYPE } from './actions'
+import { Result } from '../types/types'
+import { ACTIONTYPES } from './actions'
 
 export interface InitialStateType {
     count: number;
     page: 'setup' | 'game';
+    data: Result[];
 }
 
 export const initialState: InitialStateType = {
     count: 0,
-    page: 'setup'
+    page: 'setup',
+    data: []
 }
 
-export function reducer(state = initialState, action: ACTIONTYPE) {
+export function reducer(state = initialState, action: ACTIONTYPES): InitialStateType {
     switch (action.type) {
-        case 'increment': {
-            return { ...state, count: state.count + 1 }
-        }
-        case 'decrement': {
-            return { ...state, count: state.count - 1 }
+        case 'store_data': {
+            return {
+                ...state,
+                data: action.payload
+            }
         }
         default: {
             return state
