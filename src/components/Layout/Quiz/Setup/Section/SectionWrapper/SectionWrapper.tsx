0ac28dc14db286e15d33dd99e5662cee1f7b43d2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-25 20:41:11 +0330

    Re-write props types of SectionWrapper component

--- src/components/Layout/Quiz/Setup/Section/SectionWrapper/SectionWrapper.tsx
+++ src/components/Layout/Quiz/Setup/Section/SectionWrapper/SectionWrapper.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
+import React, { HTMLAttributes } from "react";
 import styled from "styled-components";
-import { HTMLElementAttributes } from "../../../../../../types/types";
 
 const SectionWrapperStyled = styled.div`
     width: 100%;
@@ -28,6 +27,10 @@ const SectionWrapperStyled = styled.div`
     }
 `
 
-export const SectionWrapper: ({ children, ...rest }: HTMLElementAttributes) => React.ReactElement = ({ children, ...rest }) => {
+// export const SectionWrapper: ({ children, ...rest }: HTMLElementAttributes) => React.ReactElement = ({ children, ...rest }) => {
+//     return <SectionWrapperStyled {...rest}>{children}</SectionWrapperStyled>
+// }
+
+export const SectionWrapper: React.FC = ({ children, ...rest }) => {
     return <SectionWrapperStyled {...rest}>{children}</SectionWrapperStyled>
 }
